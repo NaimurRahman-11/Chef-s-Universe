e5c5743bdfe467012d68cab69719a7406698dfd7
Naimur Rahman <nirzon.naim@gmail.com>
2023-05-03 11:26:49 -0400

    Added About us Section

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const AboutUs = () => {
     return (
         <div>
-            <h1>This is about us page</h1>
+            <div className='text-center mt-5'>
+            <h1 >Cooking is our passion!</h1>
+            <small>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vitae velit soluta, optio exercitationem, quaerat quibusdam sunt quia earum ea ad facilis asperiores! Voluptatem laudantium suscipit minus veniam natus deserunt.</small>
+            </div>
+            <img src="https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" className='img-fluid rounded w-100 mt-5 mb-5' />
         </div>
     );
 };
